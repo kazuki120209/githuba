a80ada9f18da70342d1c01b470991836f7eab0a7
kazuki120209 <sk83501229@icloud.com>
2021-06-06 22:14:53 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 小路一樹 on 2021/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/githubaApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubaApp.swift
+//  Shared
+//
+//  Created by 小路一樹 on 2021/06/06.
+//
+
+import SwiftUI
+
+@main
+struct githubaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
